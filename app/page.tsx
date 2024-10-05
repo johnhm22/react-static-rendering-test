99fd17f9532e417b6033f5e724e089f5e090a2e6
john <johnhowardm@hotmail.com>
2024-10-05 12:05:58 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
-
-export default function Home() {
+const Home = async () => {
+  const getDate = async () => new Date().toLocaleString()
+  const today = await getDate()
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>NextJS Starter Template</h1>
+        <h1 className='text-3xl font-bold'>{today}</h1>
       </div>
     </section>
   )
 }
+
+export default Home
